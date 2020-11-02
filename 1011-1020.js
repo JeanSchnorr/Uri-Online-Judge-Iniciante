5e4a613772b8b92af6e284af68f54ad95e8f1e1f
Jean Schnorr <jeanschnorr2000@gmail.com>
2020-11-02 11:09:46 -0400

    add ate 1015

--- 1011-1020.js
+++ 1011-1020.js
@@ -1,21 +1,44 @@
-const lines = ['12.7 10.4 15.2']
+const lines = ['-2.5 0.4','12.1 7.3']
 // 1011
-const pi = 3.14159
-const r = parseFloat(lines.shift())
-const v = (4/3)*pi*Math.pow(r,3) 
-console.log('VOLUME =',v.toFixed(3))
+// const pi = 3.14159
+// const r = parseFloat(lines.shift())
+// const v = (4/3)*pi*Math.pow(r,3) 
+// console.log('VOLUME =',v.toFixed(3))
 
 // 1012
-const pi = 3.14159
-const n = lines.shift().split(' ')
-const a  = parseFloat(n[0])
-const b  = parseFloat(n[1])
-const c  = parseFloat(n[2])
+// const pi = 3.14159
+// const n = lines.shift().split(' ')
+// const a  = parseFloat(n[0])
+// const b  = parseFloat(n[1])
+// const c  = parseFloat(n[2])
 
-const areaTri = ((a*c)/2).toFixed(3)
-const areaC = (pi*Math.pow(c,2)).toFixed(3)
-const areaTra = (((a+b)*c)/2).toFixed(3)
-const areaQ = (b*b).toFixed(3)
-const areaR = (a*b).toFixed(3)
+// const areaTri = ((a*c)/2).toFixed(3)
+// const areaC = (pi*Math.pow(c,2)).toFixed(3)
+// const areaTra = (((a+b)*c)/2).toFixed(3)
+// const areaQ = (b*b).toFixed(3)
+// const areaR = (a*b).toFixed(3)
 
-console.log(`TRIANGULO: ${areaTri}\nCIRCULO: ${areaC}\nTRAPEZIO: ${areaTra}\nQUADRADO: ${areaQ}\nRETANGULO: ${areaR}`)
+// console.log(`TRIANGULO: ${areaTri}\nCIRCULO: ${areaC}\nTRAPEZIO: ${areaTra}\nQUADRADO: ${areaQ}\nRETANGULO: ${areaR}`)
+
+// 1013
+// const n = lines.shift().split(" ")
+// maior = Math.max(n[0],n[1],n[2])
+// console.log(maior+' eh o maior')
+
+// 1014
+// const d = parseFloat(lines.shift())
+// const g = parseFloat(lines.shift())
+// const consumo = (d/g).toFixed(3)
+// console.log(consumo+' km/l')
+
+// 1015
+// const p1 = lines.shift().split(" ")
+// const p2 = lines.shift().split(" ")
+// const x1 = parseFloat(p1[0])
+// const y1 = parseFloat(p1[1])
+// const x2 = parseFloat(p2[0])
+// const y2 = parseFloat(p2[1])
+// d = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)).toFixed(4)
+// console.log(d)
+
+//1016
